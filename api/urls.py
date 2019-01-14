5021db74d302e2cba5dbc181b7892c9244537577
Kane Walter <kwal0203@gmail.com>
2019-01-14 17:56:52 +1100

    Updates

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 urlpatterns =[
     path('users/', include('users.urls')),
-    path('rest-auth', include('rest-auth.urls'))
+    path('rest-auth/', include('rest_auth.urls'))
 ]
